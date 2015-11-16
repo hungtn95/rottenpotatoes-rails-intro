62f2923b7361875001a726a9dddac58f13c3c7bc
hungtn95 <hungtn95@gmail.com>
2015-11-16 23:21:54 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,19 +11,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if params[:ratings] and !params[:sort_by]
-      session[:ratings] = params[:ratings]
+    @all_ratings = Movie.uniq.pluck(:rating) 
+    session[:ratings] = params[:ratings] if params[:ratings]
+    @selected_ratings = (!session[:ratings].nil? ? session[:ratings].keys : @all_ratings)
+    session[:sort_by] = params[:sort_by] if params[:sort_by]
+    @movies = Movie.order(session[:sort_by]).where(:rating => @selected_ratings)
+    if !params[:sort_by] or !params[:ratings]
       flash.keep
       redirect_to movies_path(sort_by: session[:sort_by], ratings: session[:ratings]) and return
     end   
-    if params[:sort_by] and !params[:ratings]
-      session[:sort_by] = params[:sort_by]
-      flash.keep
-      redirect_to movies_path(sort_by: session[:sort_by], ratings: session[:ratings]) and return
-    end
-    @all_ratings = Movie.uniq.pluck(:rating) 
-    @selected_ratings = (!session[:ratings].nil? ? session[:ratings].keys : @all_ratings)
-    @movies = Movie.order(session[:sort_by]).where(:rating => @selected_ratings)
   end
 
   def new
