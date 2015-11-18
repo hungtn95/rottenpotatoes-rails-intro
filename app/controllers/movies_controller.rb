88a2994a1b396c5c3f629fba43b4db01f38c5243
hungtn95 <hungtn95@gmail.com>
2015-11-18 09:31:40 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,30 +1,28 @@
 class MoviesController < ApplicationController
-
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-
   def show
+    session.clear
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @check = true
     @all_ratings = Movie.uniq.pluck(:rating) 
     session[:sort_by] = params[:sort_by] || session[:sort_by]
-    session[:ratings] = params[:ratings] || Hash[@all_ratings.map {|v| [v,1]}]
-    if !params[:ratings] or !params[:sort_by]
+    session[:ratings] = params[:ratings] || session[:ratings] 
+    if (!params[:ratings] or !params[:sort_by]) and (session[:check] == true)
+      session[:check] = false
       flash.keep
       redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) and return
     end
-    @selected_ratings = (session[:ratings] ? session[:ratings].keys : @all_ratings)
-    if session[:sort_by] != "none"
-      @movies = Movie.order(session[:sort_by]).where(:rating => @selected_ratings)
-    else
-      @movies = Movie.where(:rating => @selected_ratings)
+    if params[:ratings] or params[:sort_by]
+      session[:check] = true
     end
+    @selected_ratings = (session[:ratings] ? session[:ratings].keys : @all_ratings)
+    @movies = Movie.order(session[:sort_by]).where(:rating => @selected_ratings)
   end
 
   def new
